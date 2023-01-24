d482f4f5e1f6f24830addb2446de260182ca7bc1
unknown <pv.katherine7@gmail.com>
2023-01-24 16:38:40 +0000

    populate card class with basics

---
+++ src/Card.java
@@ -0,0 +1,50 @@
+public class Card {
+    // variables
+    private String suit;
+    private String symbol;
+    private int value;
+
+    // constructor
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    // getters and setters
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
+    // methods
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
